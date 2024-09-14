0d4f5b9a2891efd98f25a5aa02bab5b69be2a8e4
Muhammad Hesham <muhammadhisham100@gmail.com>
2024-09-14 05:29:04 +0300

    Add translation body & fav body

---
+++ lib/features/translation_feature/presentation/screens/fav_body.dart
@@ -0,0 +1,13 @@
+import 'package:bastet_app/app/widgets/text_widget.dart';
+import 'package:flutter/cupertino.dart';
+
+class FavBody extends StatelessWidget {
+  const FavBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: TextWidget(title: 'Fav Body'),
+    );
+  }
+}
---
+++ lib/features/translation_feature/presentation/screens/translation_body.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../app/utils/app_assets.dart';
+import '../../../../app/utils/app_colors.dart';
+import '../../../../app/widgets/custom_form_field.dart';
+import '../../../../app/widgets/image_widget.dart';
+import '../../../../app/widgets/text_button_widget.dart';
+import '../../../../app/widgets/text_widget.dart';
+
+class TranslationBody extends StatelessWidget {
+  const TranslationBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: Column(
+        children: [
+          20.verticalSpace,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const TextWidget(
+                title: 'هيروغليفي',
+                fontWeight: FontWeight.w400,
+              ),
+              ImageWidget(
+                imageUrl: AppAssets.arrow,
+                width: 31.w,
+                height: 15.h,
+              ),
+              const TextWidget(
+                title: 'عربي',
+                fontWeight: FontWeight.w400,
+              ),
+            ],
+          ),
+          12.verticalSpace,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
+            height: 200.h,
+            decoration: BoxDecoration(
+              color: AppColors.containerColor,
+              borderRadius: BorderRadius.circular(6.r),
+            ),
+            child: Column(
+              children: [
+                const CustomFormField(
+                  hint: 'اكتب  هنا',
+                  minLines: 2,
+                  maxLines: 5,
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 48.h,
+                  child: Row(
+                    children: [
+                      CustomTextButton(
+                        onPressed: () {},
+                        icon: ImageWidget(
+                          imageUrl: AppAssets.audio,
+                          width: 24.w,
+                          height: 20.h,
+                        ),
+                      ),
+                      VerticalDivider(
+                        width: 8.w,
+                        color: AppColors.colorC7B384,
+                        indent: 10.h,
+                        endIndent: 10.h,
+                      ),
+                      CustomTextButton(
+                        onPressed: () {},
+                        icon: ImageWidget(
+                          imageUrl: AppAssets.microphone,
+                          width: 14.w,
+                          height: 22.h,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          16.verticalSpace,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
+            height: 270.h,
+            decoration: BoxDecoration(
+              color: AppColors.containerColor,
+              borderRadius: BorderRadius.circular(6.r),
+              border: Border.all(color: AppColors.colorA69670),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextWidget(
+                      title: 'هيروغليفي',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16.sp,
+                      color: AppColors.hint,
+                    ),
+                    4.horizontalSpace,
+                    CustomTextButton(
+                      onPressed: () {},
+                      icon: ImageWidget(
+                        imageUrl: AppAssets.audio,
+                        width: 24.w,
+                        height: 20.h,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextWidget(
+                      title: '𓊖',
+                      color: AppColors.primaryColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 22.sp,
+                    ),
+                    5.horizontalSpace,
+                    const TextWidget(
+                      title: 'كيميت',
+                      color: AppColors.primaryColor,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    10.horizontalSpace,
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextWidget(
+                      title: '𓊖',
+                      color: Colors.transparent,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 22.sp,
+                    ),
+                    5.horizontalSpace,
+                    const TextWidget(
+                      title: 'كيميت',
+                      color: Colors.transparent,
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.overline,
+                    ),
+                    10.horizontalSpace,
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10.w),
+                  child: const TextWidget(
+                    title: 'مصر',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const Spacer(),
+                Divider(
+                  color: AppColors.color84744F,
+                  indent: 10.w,
+                  endIndent: 10.w,
+                ),
+                SizedBox(
+                  height: 48.h,
+                  child: Row(
+                    children: [
+                      CustomTextButton(
+                        onPressed: () {},
+                        icon: ImageWidget(
+                          imageUrl: AppAssets.share,
+                          width: 20.w,
+                          height: 20.h,
+                        ),
+                      ),
+                      VerticalDivider(
+                        width: 8.w,
+                        color: AppColors.colorC7B384,
+                        indent: 10.h,
+                        endIndent: 10.h,
+                      ),
+                      CustomTextButton(
+                        onPressed: () {},
+                        icon: ImageWidget(
+                          imageUrl: AppAssets.fav,
+                          width: 20.w,
+                          height: 20.h,
+                        ),
+                      ),
+                      VerticalDivider(
+                        width: 8.w,
+                        color: AppColors.colorC7B384,
+                        indent: 10.h,
+                        endIndent: 10.h,
+                      ),
+                      CustomTextButton(
+                        onPressed: () {},
+                        icon: ImageWidget(
+                          imageUrl: AppAssets.copy,
+                          width: 20.w,
+                          height: 20.h,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
