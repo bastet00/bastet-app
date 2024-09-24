23752525f39f74b7dd97ff33b681ced06b909d2c
Muhammad Hesham <muhammadhisham100@gmail.com>
2024-09-24 14:22:25 +0300

    Add privacy policy screen

---
+++ lib/features/translation_feature/presentation/screens/privacy_policy_screen.dart
@@ -0,0 +1,44 @@
+import 'package:bastet_app/app/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+
+
+import '../../../../app/utils/app_colors.dart';
+import '../../../../app/widgets/loading.dart';
+import '../presentation_logic_holder/settings_cubit/settings_cubit.dart';
+
+class PrivacyPolicyScreen extends StatelessWidget {
+  const PrivacyPolicyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: AppColors.primaryColor),
+        backgroundColor: const Color(0xFF2C2924),
+        title: const TextWidget(
+          title: 'سياسة الخصوصية', color: AppColors.primaryColor,),
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsetsDirectional.all(16.r),
+        child: BlocBuilder<SettingsCubit, SettingsState>(
+          builder: (context, state) {
+            return state is SettingsLoading
+            ? const Loading ()
+            : Center(
+              child: HtmlWidget(
+                SettingsCubit.get().privacyPolicy ?? 'غير موجود',
+                textStyle: const TextStyle(
+                  color: AppColors.white,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
