50ad2bfd945d8f6cfa461e003782bb802c0f1fe4
Muhammad Hesham <muhammadhisham100@gmail.com>
2024-11-07 15:57:46 +0200

    Add navigation to details screen when tapping translation container

--- lib/features/translation_feature/presentation/widgets/translation_widget.dart
+++ lib/features/translation_feature/presentation/widgets/translation_widget.dart
@@ -39,160 +39,166 @@ class TranslationWidget extends StatelessWidget {
     final hieroglyphics = translationDetails?.egyptian?[0].hieroglyphics?.map((e) => e).join(', ');
     final resources = translationDetails?.resources?.map((e) => '• $e').join('\n');
 
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
-      decoration: BoxDecoration(
-        color: AppColors.containerColor,
-        borderRadius: BorderRadius.circular(6.r),
-        border: Border.all(color: AppColors.colorA69670),
-      ),
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Row(
-                //   children: [
-                //     // CustomTextButton(
-                //     //   onPressed: () {},
-                //     //   icon: ImageWidget(
-                //     //     imageUrl: AppAssets.audio,
-                //     //     width: 24.w,
-                //     //     height: 20.h,
-                //     //   ),
-                //     // ),
-                //     TextWidget(
-                //       title: TranslationCubit.get().fromArabic ? 'هيروغليفي' : 'عربي',
-                //       fontWeight: FontWeight.w400,
-                //       fontSize: 16.sp,
-                //       color: AppColors.hint,
-                //     ),
-                //   ],
-                // ),
-                20.horizontalSpace,
-                Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    TextWidget(
-                      title: egyptianWord?? '',
-                      color: Colors.transparent,
-                      fontWeight: FontWeight.w700,
-                      underLine: true,
-                      fontSize: isDetailsScreen ? 22.sp : 18.sp,
+    return InkWell(
+      onTap: () {
+        if (translation?.id != null) navigateTo(TranslationDetailsScreen(translation: translation!));
+      },
+      borderRadius: BorderRadius.circular(6.r),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
+        decoration: BoxDecoration(
+          color: AppColors.containerColor,
+          borderRadius: BorderRadius.circular(6.r),
+          border: Border.all(color: AppColors.colorA69670),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Row(
+                  //   children: [
+                  //     // CustomTextButton(
+                  //     //   onPressed: () {},
+                  //     //   icon: ImageWidget(
+                  //     //     imageUrl: AppAssets.audio,
+                  //     //     width: 24.w,
+                  //     //     height: 20.h,
+                  //     //   ),
+                  //     // ),
+                  //     TextWidget(
+                  //       title: TranslationCubit.get().fromArabic ? 'هيروغليفي' : 'عربي',
+                  //       fontWeight: FontWeight.w400,
+                  //       fontSize: 16.sp,
+                  //       color: AppColors.hint,
+                  //     ),
+                  //   ],
+                  // ),
+                  20.horizontalSpace,
+                  Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      TextWidget(
+                        title: egyptianWord?? '',
+                        color: Colors.transparent,
+                        fontWeight: FontWeight.w700,
+                        underLine: true,
+                        fontSize: isDetailsScreen ? 22.sp : 18.sp,
+                      ),
+                      TextWidget(
+                        title: egyptian,
+                        color: AppColors.primaryColor,
+                        fontWeight: FontWeight.w700,
+                        fontSize: isDetailsScreen ? 22.sp : 18.sp,
+                      ),
+                    ],
+                  ),
+                  12.verticalSpace,
+                  if (!isDetailsScreen) TextWidget(
+                    title: arabic?? '',
+                    fontWeight: FontWeight.w400,
+                    titleAlign: TextAlign.start,
+                    maxLines: 6,
+                  ),
+                  if (isDetailsScreen) ...[
+                    DetailsColumnWidget(
+                      title: 'إنجليزي:',
+                      description: english,
                     ),
-                    TextWidget(
-                      title: egyptian,
-                      color: AppColors.primaryColor,
-                      fontWeight: FontWeight.w700,
-                      fontSize: isDetailsScreen ? 22.sp : 18.sp,
+                    DetailsColumnWidget(
+                      title: 'عربي:',
+                      description: arabic,
+                    ),
+                    DetailsColumnWidget(
+                      title: 'القيمة الصوتية الإنجليزية:',
+                      description: translationDetails?.egyptian?[0].transliteration,
+                    ),
+                    DetailsColumnWidget(
+                      title: 'الهيروغليفية:',
+                      description: hieroglyphicSigns,
+                      descriptionFontSize: 16.sp,
+                    ),
+                    DetailsColumnWidget(
+                      title: 'علامات جاردنير:',
+                      description: hieroglyphics,
+                    ),
+                    DetailsColumnWidget(
+                      title: 'المصادر:',
+                      description: resources,
+                      descriptionFontSize: 12.sp,
                     ),
                   ],
-                ),
-                12.verticalSpace,
-                if (!isDetailsScreen) TextWidget(
-                  title: arabic?? '',
-                  fontWeight: FontWeight.w400,
-                  titleAlign: TextAlign.start,
-                  maxLines: 6,
-                ),
-                if (isDetailsScreen) ...[
-                  DetailsColumnWidget(
-                    title: 'إنجليزي:',
-                    description: english,
-                  ),
-                  DetailsColumnWidget(
-                    title: 'عربي:',
-                    description: arabic,
-                  ),
-                  DetailsColumnWidget(
-                    title: 'القيمة الصوتية الإنجليزية:',
-                    description: translationDetails?.egyptian?[0].transliteration,
-                  ),
-                  DetailsColumnWidget(
-                    title: 'الهيروغليفية:',
-                    description: hieroglyphicSigns,
-                    descriptionFontSize: 16.sp,
-                  ),
-                  DetailsColumnWidget(
-                    title: 'علامات جاردنير:',
-                    description: hieroglyphics,
-                  ),
-                  DetailsColumnWidget(
-                    title: 'المصادر:',
-                    description: resources,
-                    descriptionFontSize: 12.sp,
+                  20.verticalSpace,
+                  const Divider(
+                    color: AppColors.color84744F,
                   ),
                 ],
-                20.verticalSpace,
-                const Divider(
-                  color: AppColors.color84744F,
-                ),
-              ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 48.h,
-            child: Row(
-              children: [
-                if (!isDetailsScreen) CustomTextButton(
-                  onPressed: () {
-                    if (translation?.id != null) navigateTo(TranslationDetailsScreen(translation: translation!));
-                  },
-                  title: 'القاموس',
-                  titleColor: AppColors.white,
-                  icon: ImageWidget(
-                    imageUrl: AppAssets.dictionary,
-                    width: 16.w,
-                    height: 20.h,
+            SizedBox(
+              height: 48.h,
+              child: Row(
+                children: [
+                  if (!isDetailsScreen) CustomTextButton(
+                    onPressed: () {
+                      if (translation?.id != null) navigateTo(TranslationDetailsScreen(translation: translation!));
+                    },
+                    title: 'القاموس',
+                    titleColor: AppColors.white,
+                    icon: ImageWidget(
+                      imageUrl: AppAssets.dictionary,
+                      width: 16.w,
+                      height: 20.h,
+                    ),
                   ),
-                ),
-                const Spacer(),
-                CustomTextButton(
-                  onPressed: () {
-                    // Copy to clipboard
-                    Clipboard.setData(ClipboardData(text: egyptian));
-                    showToast(msg: 'تم النسخ');
-                  },
-                  icon: ImageWidget(
-                    imageUrl: AppAssets.copy,
-                    width: 20.w,
-                    height: 20.h,
+                  const Spacer(),
+                  CustomTextButton(
+                    onPressed: () {
+                      // Copy to clipboard
+                      Clipboard.setData(ClipboardData(text: egyptian));
+                      showToast(msg: 'تم النسخ');
+                    },
+                    icon: ImageWidget(
+                      imageUrl: AppAssets.copy,
+                      width: 20.w,
+                      height: 20.h,
+                    ),
                   ),
-                ),
-                VerticalDivider(
-                  width: 8.w,
-                  color: AppColors.colorC7B384,
-                  indent: 10.h,
-                  endIndent: 10.h,
-                ),
-                FavWidget(
-                  translation: translation!,
-                ),
-                VerticalDivider(
-                  width: 8.w,
-                  color: AppColors.colorC7B384,
-                  indent: 10.h,
-                  endIndent: 10.h,
-                ),
-                CustomTextButton(
-                  onPressed: () async {
-                    final id = translation?.id;
-                    final String appLink = 'bastet-app.com/word/$id';
-                    Clipboard.setData(ClipboardData(text: appLink));
-                    Share.share(appLink);
-                  },
-                  icon: ImageWidget(
-                    imageUrl: AppAssets.share,
-                    width: 20.w,
-                    height: 20.h,
+                  VerticalDivider(
+                    width: 8.w,
+                    color: AppColors.colorC7B384,
+                    indent: 10.h,
+                    endIndent: 10.h,
                   ),
-                ),
-              ],
+                  FavWidget(
+                    translation: translation!,
+                  ),
+                  VerticalDivider(
+                    width: 8.w,
+                    color: AppColors.colorC7B384,
+                    indent: 10.h,
+                    endIndent: 10.h,
+                  ),
+                  CustomTextButton(
+                    onPressed: () async {
+                      final id = translation?.id;
+                      final String appLink = 'bastet-app.com/word/$id';
+                      Clipboard.setData(ClipboardData(text: appLink));
+                      Share.share(appLink);
+                    },
+                    icon: ImageWidget(
+                      imageUrl: AppAssets.share,
+                      width: 20.w,
+                      height: 20.h,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
