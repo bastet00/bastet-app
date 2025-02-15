8ea205d18d1eadfb42d8d2d15c9d91935fa9ae4d
Amro <amranwar945@gmail.com>
2025-02-16 00:13:42 +0100

    Add mapper

--- lib/features/translation_feature/presentation/widgets/literal_translation_widget.dart
+++ lib/features/translation_feature/presentation/widgets/literal_translation_widget.dart
@@ -18,104 +18,119 @@ import '../../../../app/widgets/text_widget.dart';
 class LiteralTranslationWidget extends StatefulWidget {
   final LiteralTranslationModel? literalTranslation;
 
-
   const LiteralTranslationWidget({
     super.key,
     this.literalTranslation,
   });
 
-
   @override
   State<LiteralTranslationWidget> createState() => _LiteralTranslationWidget();
 }
-class _LiteralTranslationWidget extends State<LiteralTranslationWidget>{
+
+class _LiteralTranslationWidget extends State<LiteralTranslationWidget> {
   bool isFolded = true;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: 1.w,
-        vertical: 1.h,
-      ),
-      decoration: BoxDecoration(
-        color: AppColors.containerColor,
-        borderRadius: BorderRadius.circular(6.r),
-        border: Border.all(color: AppColors.colorA69670),
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              CustomTextButton(
-                onPressed: () {
-                  setState(() {
-                    isFolded = !isFolded;
-                  });
-                },
-                icon: Padding(
-                  padding: EdgeInsets.only(left: 20.w),
-                  child: ImageWidget(
-                    imageUrl: isFolded ? AppAssets.foldedArrow : AppAssets.unfoldedArrow,
+        padding: EdgeInsets.symmetric(vertical: 1.h),
+        decoration: BoxDecoration(
+          color: AppColors.containerColor,
+          borderRadius: BorderRadius.circular(6.r),
+          border: Border.all(color: AppColors.colorA69670),
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CustomTextButton(
+                  onPressed: () {
+                    setState(() {
+                      isFolded = !isFolded;
+                    });
+                  },
+                  title: 'ترجمة حرفية',
+                  titleColor: AppColors.suggestionsTextColor,
+                  fontWeight: FontWeight.w400,
+                  icon: ImageWidget(
+                    imageUrl: isFolded
+                        ? AppAssets.foldedArrow
+                        : AppAssets.unfoldedArrow,
                     width: 25.w,
                     height: 25.h,
                   ),
                 ),
-              ),
-              10.horizontalSpace,
-              const Align(
-                alignment: Alignment.centerRight,
-                child: TextWidget(
-                  title: "ترجمة حرفية",
-                  maxLines: 5,
-                  color: AppColors.suggestionsTextColor,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: TextWidget(
-                    title: widget.literalTranslation?.literalTranslation ?? '',
-                    maxLines: 5,
-                    fontSize: 20.sp,
-                    fontWeight: FontWeight.w500,
+
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: TextWidget(
+                      title:
+                          widget.literalTranslation?.literalTranslation ?? '',
+                      maxLines: 5,
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
-              ),
-              20.horizontalSpace,
-              CustomTextButton(
-                onPressed: () {
-                  Fluttertoast.cancel();
-                  // Copy to clipboard
-                  Clipboard.setData(ClipboardData(text: widget.literalTranslation?.literalTranslation ?? ''));
-                  showToast(msg: 'تم النسخ');
-                },
-                icon: ImageWidget(
-                  imageUrl: AppAssets.copy,
-                  width: 20.w,
-                  height: 20.h,
-                ),
-              ),
-            ],
-          ),
-          if (!isFolded) ...[
-            Row(
-              children: [
-                TextWidget(
-                  title: widget.literalTranslation?.literalTranslation ?? '',
-                  fontWeight: FontWeight.w400,
-                  titleAlign: TextAlign.start,
-                  maxLines: 10,
-                  fontSize: 14.sp,
+                CustomTextButton(
+                  onPressed: () {
+                    Fluttertoast.cancel();
+                    // Copy to clipboard
+                    Clipboard.setData(ClipboardData(
+                        text: widget.literalTranslation?.literalTranslation ??
+                            ''));
+                    showToast(msg: 'تم النسخ');
+                  },
+                  icon: ImageWidget(
+                    imageUrl: AppAssets.copy,
+                    width: 15.w,
+                    height: 15.h,
+                  ),
                 ),
               ],
-            )
-
-        ]
-        ],
-      )
-    );
+            ),
+            if (!isFolded) ...[
+              Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 5.w, vertical: 10.w),
+                  child: Column(
+                    children: [
+                      const Divider(
+                        color: AppColors.color84744F,
+                      ),
+                      SingleChildScrollView(
+                        reverse: true,
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                              children: [
+                            if (widget.literalTranslation?.lettersMapper !=
+                                null)
+                              ...?widget.literalTranslation!.lettersMapper?.map(
+                                  (letterMap) => Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 10.w),
+                                      child: Column(
+                                        children: [
+                                          TextWidget(
+                                            title:
+                                                letterMap.hieroglyphics ?? '',
+                                            fontWeight: FontWeight.w400,
+                                            titleAlign: TextAlign.start,
+                                          ),
+                                          10.verticalSpace,
+                                          TextWidget(
+                                            title:
+                                                letterMap.alphabetLetters ?? '',
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ],
+                                      )))
+                          ])),
+                    ],
+                  ))
+            ]
+          ],
+        ));
   }
-
 }
