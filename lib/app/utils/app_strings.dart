76f5ba64c49435cd9f03a554ffa53b7f2d7223cf
Muhammad Hesham <muhammadhisham100@gmail.com>
2025-06-14 20:04:16 +0300

    Add: translation

--- lib/app/utils/app_strings.dart
+++ lib/app/utils/app_strings.dart
@@ -1,157 +1,29 @@
 class AppStrings {
-  /// Auth Feature
-  static const String login = 'login';
-  static const String loginTitle = 'login_title';
-  static const String loginSuccess = 'login_success';
-  static const String signUp = 'sign_up';
-  static const String registerNow = 'register_now';
-  static const String registerTitle = 'register_title';
-  static const String forgetPassword = 'forget_password';
-  static const String forgetPasswordTitle = 'forget_password_title';
-  static const String haveAccount = 'have_account';
-  static const String haveNoAccount = 'have_no_account';
-  static const String fullName = 'full_name';
-  static const String enterFullName = 'enter_full_name';
-  static const String email = 'email';
-  static const String enterEmail = 'enter_email';
-  static const String phoneNumber = 'phone_number';
-  static const String enterPhoneNumber = 'enter_phone_number';
-  static const String password = 'password';
-  static const String enterPassword = 'enter_password';
-  static const String confirmPassword = 'confirm_password';
-  static const String enterConfirmPassword = 'enter_confirm_password';
-  static const String otp = 'otp';
-  static const String sendOtp = 'send_otp';
-  static const String resendOtp = 'resend_otp';
-  static const String otpSent = 'otp_sent';
-  static const String activeOtp = 'active_otp';
-  static const String enterOtp = 'enter_otp';
-  static const String otpTitle = 'otp_title';
-  static const String notReceivedOtp = 'not_received_otp';
-  static const String newPassword = 'new_password';
-  static const String enterNewPassword = 'enter_new_password';
-  static const String confirm = 'confirm';
-  static const String resetPassword = 'reset_password';
-  static const String changePassword = 'change_password';
-  static const String oldPassword = 'old_password';
-  static const String resetPasswordTitle = 'reset_password_title';
-  static const String resetPasswordSuccess = 'reset_password_success';
-  static const String search = 'search';
-  static const String wrongEmail = "wrong_email";
-  static const String passwordMismatch = "password_mismatch";
-  static const String successfullyDone = 'successfully_done';
-  static const String minute = "minute";
-  static const String incompleteData = "incomplete_data";
-  static const String or = 'or';
-  static const String continueWith = 'continue_with';
-
-
-  /// Profile Feature
-  static const String profile = "profile";
-  static const String language = 'language';
-  static const String chooseLanguage = 'choose_language';
-  static const String english = 'English';
-  static const String arabic = 'العربية';
-  static const String about = 'about';
-  static const String contactUs = 'contact_us';
-  static const String editProfile = 'edit_profile';
-  static const String termsAndConditions = 'terms_and_conditions';
-  static const String privacyPolicy = 'privacy_policy';
-  static const String logout = 'logout';
-  static const String deleteAccount = 'delete_account';
-  static const String sureDelete = 'sure_delete';
-  static const String yes = 'yes';
-  static const String no = 'no';
-  static const String cancel = 'cancel';
-  static const String save = 'save';
-  static const String notFound = 'not_found';
-  static const String couldNotLaunch = 'could_not_launch';
-  static const String view = 'view';
-  static const String edit = 'edit';
-  static const String delete = 'delete';
-  static const String remove = 'remove';
+  /// App
+  static const String ok = "ok";
   static const String requiredField = "required_field";
   static const String mustLessThan = "must_less_than";
   static const String mustMoreThan = "must_more_than";
   static const String digits = "digits";
   static const String characters = "characters";
-  static const String invalidPhone = "invalid_phone";
-  static const String firstPage = 'first_page';
-  static const String noPrevious = 'no_previous';
-  static const String noNext = 'no_next';
-  static const String lastPage = 'last_page';
-  static const String subject = 'subject';
-  static const String message = 'message';
-  static const String subscribe = 'subscribe';
-  static const String dateOfBirth = 'date_of_birth';
+  static const String arabic = "العربية";
+  static const String english = "English";
 
-
-  /// Home Feature
-  static const String home = "home";
-  static const String packages = 'packages';
-  static const String selectPackage = 'select_package';
-  static const String packageTitle = 'package_title';
-  static const String services = 'services';
-  static const String details = 'details';
-  static const String searchForServiceProvider = 'search_for_service_provider';
-  static const String emptyProviders = 'empty_providers';
-  static const String addedSuccessfullyToFav = 'added_successfully_to_fav';
-  static const String removedSuccessfullyFromFav = 'removed_successfully_from_fav';
-  static const String addedSuccessfullyToPro = 'added_successfully_to_pro';
-  static const String removedSuccessfullyFromPro = 'removed_successfully_from_pro';
-  static const String favourites = 'favourites';
-  static const String promotions = 'promotions';
-  static const String addToFavourite = 'add_to_favourite';
-  static const String deleteFromFavourite = 'delete_from_favourite';
-
-  /// Blogs Feature
-  static const String blogs = 'blogs';
-  static const String addComment = 'add_comment';
-  static const String addYourComment = 'add_your_comment';
-  static const String sendComment = 'send_comment';
-  static const String emptyBlogs = 'empty_blogs';
-
-  /// Network Feature
-  static const String ourNetwork = 'our_network';
-  static const String joinToOurNetwork = 'join_to_our_network';
-  static const String companyName = 'company_name';
-  static const String companyWebsite = 'company_website';
-  static const String region = 'region';
-  static const String city = 'city';
-  static const String numberOfEmployees = 'number_of_employees';
-  static const String sector = 'sector';
-  static const String jobTitle = 'job_title';
-  static const String administratorJobTitle = 'administrator_job_title';
-  static const String administratorPhone = 'administrator_phone';
-  static const String administratorEmail = 'administrator_email';
-  static const String otherDetails = 'other_details';
-  static const String optional = 'optional';
-  static const String send = 'send';
-  static const String category = 'category';
-  static const String pleaseSelectCity = 'please_select_city';
-  static const String pleaseSelectCard = 'please_select_card';
-  static const String pleaseSelectCategory = 'please_select_category';
-
-
-
-
-
-  static const String myProfile = 'my_profile';
-  static const String ok = "ok";
-  static const String address = 'address';
-  static const String addressDetails = 'address_details';
-  static const String googleMaps = 'google_maps';
-  static const String phoneNo = 'phone_no';
-  static const String socialChannels = 'social_channels';
-  static const String writeYourMessage = 'write_your_message';
-  static const String yourMessage = 'your_message';
-  static const String messageSentSuccessfully = 'message_sent_successfully';
-  static const String createYourAccount = 'create_your_account';
+  /// Intro Feature
+  static const String onBoarding1 = 'on_boarding_1';
+  static const String onBoarding2 = 'on_boarding_2';
+  static const String onBoarding3 = 'on_boarding_3';
   static const String skip = 'skip';
-  static const String orContinueWith = 'or_continue_with';
-  static const String verifyPhone = "verify_phone";
-  static const String iForgotPassword = "i_forgot_password";
-  static const String changeNow = "change_now";
-  static const String welcome = "welcome";
-  static const String createdAccount = "created_account";
+  static const String welcome = 'welcome';
+  static const String start = 'start';
+  static const String previous = 'previous';
+  static const String next = 'next';
+
+  /// Settings Feature
+  static const String shareApp = 'share_app';
+  static const String rateApp = 'rate_app';
+  static const String privacyPolicy = 'privacy_policy';
+  static const String suggestIdea = 'suggest_idea';
+  static const String contactUs = 'contact_us';
+  static const String donate = 'donate';
 }
